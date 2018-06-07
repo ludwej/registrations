806a94368b767073f1a98d0dd87206aec00f4654
ludwej <jacobsludwe84@gmail.com>
2018-06-07 10:09:12 +0200

    started with registrations

---
+++ JS/Registration-dom.js
@@ -0,0 +1,6 @@
+var input = document.querySelector('.Input')
+var AddBTN = document.querySelector('.AddBTN')
+var ClearBTN = document.querySelector('.ClearBTN')
+var Town = document.querySelector('.Town')
+var Output = document.querySelector('.Output')
+var input = document.querySelector('.Input')
---
+++ JS/Registration.js
@@ -0,0 +1,19 @@
+function regNumbers() {
+
+
+
+var town = ['Cape Town' , 'Paarl' , 'George', 'Stellenbosch' ]
+
+
+function displayEachRegNumber (list)
+{
+
+  var newList = list
+  for (var i=0 ; i<list.length;i++)
+ console.log(newList[i])
+
+ if regNo[i] startsWith('CA')
+}
+  return Cape Town
+
+}
